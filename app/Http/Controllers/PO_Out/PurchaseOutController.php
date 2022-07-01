c6d97daa2d2e7bd35384e84650edde9d86881703
Stefanus Likardi <stefanus.likardi@nutrifood.co.id>
2022-07-01 21:13:37 +0700

    [other] add controller

---
+++ app/Http/Controllers/PO_Out/PurchaseOutController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\PO_Out;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PurchaseOutController extends Controller
+{
+    //
+}
